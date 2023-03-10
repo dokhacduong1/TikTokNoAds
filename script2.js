88d44ef2cdef4b435f6382b4eb22aa301395d4fa
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-03-10 10:20:03 +0700

    UpdateV6

--- script2.js
+++ script2.js
@@ -2,11 +2,12 @@
 // var encodedUrlTiktok = encodeURIComponent(inputTiktok);
 const options = {
     method: 'GET',
-    headers:{
-        'Access-Control-Allow-Origin': '127.0.0.1:5500'
+    params:{
+        'url' : 'https://www.tiktok.com/@fungame_office/video/7208561324261969198',
+        'minimal' : false
     }
 };
-fetch(`https://api.douyin.wtf/api?url=https://www.tiktok.com/@fungame_office/video/7208561324261969198&minimal=false`)
+fetch(`https://api.douyin.wtf/api`,options)
 .then((response) => {
     return response.json();
 }).then((response)=>{
